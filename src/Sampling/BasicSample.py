0118cf42005d2c43fed330a342dfa3286fa4c65b
Robbie <rw1812@ic.ac.uk>
2019-08-01 23:45:32 +0100

    added some more samplers

--- src/Sampling/BasicSample.py
+++ src/Sampling/BasicSample.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import random as rand
+from ..Misc.Distributions import binomial
 
 
 class Sample:
@@ -48,6 +49,48 @@ class Uniform(Sample):
                 break
 
 
+class Binomial(Sample):
+    """
+    Class for sampling from a binomial distribution.
+    """
+
+    def __init__(self, n, p):
+        """
+        :param n: Number of tries
+        :param p: Probability of event
+        """
+        if not isinstance(n, int) or n < 0:
+            raise ValueError("n must be positive int!")
+        if p < 0. or p > 1.:
+            raise ValueError("p must be probability!")
+        self.n = n
+        self.p = p
+
+        # generate cdf
+        self.cdf = np.zeros(n)
+        self.cdf[0] = binomial(n, p, 0)
+        for i in range(1, n):
+            self.cdf[i] = binomial(n, p, i) + self.cdf[i-1]
+
+    def generate(self, end=1e6):
+        """
+        Generates a sequence of binomial samples
+        :param end: End of generator
+        :return Generator of binomial numbers
+        """
+
+        i = 0
+        while True:
+            i += 1
+            r = rand.uniform(0., 1.)
+            for k, bound in enumerate(self.cdf):
+                if bound > r:
+                    yield k
+                    break
+            if i == end:
+                break
+
+
 class Gaussian1d(Sample):
     """
     Gaussian random number generator Box–Muller transform
@@ -76,8 +119,52 @@ class Gaussian1d(Sample):
             yield (gauss1 - self.mu) / self.sigma
             if i == end:
                 break
-            gauss2 = (-2.0 * math.log(r2))**0.5 * math.sin(2. * math.pi * r1)
+            gauss2 = (-2.0 * math.log(r1))**0.5 * math.sin(2. * math.pi * r2)
             i += 1
             yield (gauss2 - self.mu) / self.sigma
+            if i == end:
+                break
+
+
+class GaussianNd(Sample):
+    """
+    Multivariate Gaussian random number might fail semideninite
+    matrix.
+    """
+
+    def __init__(self, mu, covar):
+        """
+        :param mu: Numpy array of mean of gaussian
+        :param covar: Covariance matrix of gaussian
+        """
+        # Check inputs are good
+        if not isinstance(mu, np.ndarray):
+            raise ValueError("mu not numpy array!")
+        if not isinstance(covar, np.ndarray):
+            raise ValueError("mu not numpy array!")
+        if len(mu.shape) != 1 or len(covar.shape) != 2 \
+                or mu.shape[0] != covar.shape[0] \
+                or mu.shape[0] != covar.shape[1]:
+            raise ValueError("Inputs bad shape: {} and {}".format(mu.shape, covar.shape))
+
+        self.mu = mu
+        self.sigma = covar
+        self.cholesky = np.linalg.cholesky(covar)
+        self.gauss_1d = Gaussian1d(0, 1)
+        print(self.cholesky)
+
+    def generate(self, end=1e6):
+        """
+        Generates a sequence of random gaussian numbers
+        :param end: End of generator
+        :return Generator of random numbers
+        """
+        i = 0
+        while True:
+            # Generate standard gaussian samples
+            z = self.gauss_1d.sample(self.mu.shape[0])
+
+            i += 1
+            yield self.mu + self.cholesky @ z
             if i == end:
                 break
