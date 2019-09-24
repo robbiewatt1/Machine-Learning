d198da3eb785651a96c94e6fc9dfd2c9773b3a8b
Robbie <rw1812@ic.ac.uk>
2019-09-24 22:10:15 +0100

    minor name change

--- src/Misc/Distributions.py
+++ src/Misc/Distributions.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 
-def gaussian(x, mu, sig):
+def gaussian1d(x, mu, sig):
     """
     Gaussian density function
     :param x: position
@@ -13,9 +13,21 @@ def gaussian(x, mu, sig):
     return (2.0 * np.pi * sig**2.0)**-0.5 * np.exp(-(x - mu)**2.0 / (2.0 * sig**2.0))
 
 
+def gaussian_nd(x, mu, covar):
+    """
+    Multivariate Gaussian density function
+    :param x: position
+    :param mu: mean
+    :param covar: covariance
+    :return: gaussian density
+    """
+    return np.exp(-0.5 * (x - mu) @ np.linalg.inv(covar) @ (x - mu))\
+        * ((2.0 * np.pi) ** x.size * np.linalg.det(covar)) ** -0.5
+
+
 def uniform(x, mu, h):
     """
-    Unifrom density function
+    Uniform density function
     :param x: position
     :param mu: distribution centre
     :param h: distribution width
